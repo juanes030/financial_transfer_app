b73787063033a7b5c2398287e396f6828cd2d776
Juan Esteban Alvarez Ruiz <juanes030@hotmail.com>
2026-07-25 23:36:46 -0500

    Configure dependency injection

---
+++ lib/features/users/data/models/user_model.dart
@@ -0,0 +1,40 @@
+import 'package:financial_transfer_app/features/users/domain/entities/user_entity.dart';
+import 'package:financial_transfer_app/features/users/domain/entities/user_role.dart';
+
+class UserModel extends UserEntity {
+  const UserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+    required super.password,
+    required super.balance,
+    required super.role,
+    required super.createdAt,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      balance: json['balance'] as int,
+      role: UserRole.values.firstWhere(
+        (role) => role.name == json['role'],
+      ),
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'balance': balance,
+      'role': role.name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:financial_transfer_app/app/app.dart';
+import 'package:financial_transfer_app/injection/injection.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await configureDependencies();
+
   runApp(const App());
 }
